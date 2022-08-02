09b8b4b9b93a347da7515cf54154dcb29da3324d
Riccardo Gulin <riccardo@nucleode.com>
2022-08-02 10:47:21 +0200

    Adds basic server;adds schema&model; adds POST&GET

---
+++ src/api/users/index.js
@@ -0,0 +1,47 @@
+import express from "express"
+import UsersModel from "./model.js"
+
+const usersRouter = express.Router()
+
+usersRouter.post("/", async (req, res, next) => {
+  try {
+    const newUser = new UsersModel(req.body) // here it happens the validation (thanks to Mongoose) of request body, if it is not ok Mongoose will throw an error (if it is ok it is NOT saved yet)
+    const { _id } = await newUser.save()
+
+    res.status(201).send({ _id })
+  } catch (error) {
+    next(error)
+  }
+})
+
+usersRouter.get("/", async (req, res, next) => {
+  try {
+    const users = await UsersModel.find()
+    res.send(users)
+  } catch (error) {
+    next(error)
+  }
+})
+
+usersRouter.get("/:userId", async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error)
+  }
+})
+
+usersRouter.put("/:userId", async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error)
+  }
+})
+
+usersRouter.delete("/:userId", async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error)
+  }
+})
+
+export default usersRouter
