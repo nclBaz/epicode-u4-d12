e8335c4f7eed54262ec75ba867c8da604f5a1772
Riccardo Gulin <riccardo@nucleode.com>
2022-08-02 11:31:09 +0200

    Adds GET,PUT,DELETE /:userId

--- src/api/users/index.js
+++ src/api/users/index.js
@@ -1,4 +1,5 @@
 import express from "express"
+import createHttpError from "http-errors"
 import UsersModel from "./model.js"
 
 const usersRouter = express.Router()
@@ -25,6 +26,12 @@ usersRouter.get("/", async (req, res, next) => {
 
 usersRouter.get("/:userId", async (req, res, next) => {
   try {
+    const user = await UsersModel.findById(req.params.userId)
+    if (user) {
+      res.send(user)
+    } else {
+      next(createHttpError(404, `User with id ${req.params.userId} not found!`))
+    }
   } catch (error) {
     next(error)
   }
@@ -32,6 +39,27 @@ usersRouter.get("/:userId", async (req, res, next) => {
 
 usersRouter.put("/:userId", async (req, res, next) => {
   try {
+    const updatedUser = await UsersModel.findByIdAndUpdate(
+      req.params.userId, // WHO you want to modify
+      req.body, // HOW you want to modify
+      { new: true, runValidators: true } // OPTIONS. By default findByIdAndUpdate returns the record pre-modification. If you want to get back the newly update record you should use the option new: true
+      // By default validation is off here --> runValidators: true
+    )
+    // ************************************************* ALTERNATIVE METHOD *******************************************************
+
+    // const user = await UsersModel.findById(req.params.userId) // when you do a findById, findOne, etc,... you get back a MONGOOSE DOCUMENT which is NOT a normal object but an object with some superpowers (like the .save() method) that will be useful in the future
+
+    // user.firstName = "Diego"
+
+    // await user.save()
+
+    // res.send(user)
+
+    if (updatedUser) {
+      res.send(updatedUser)
+    } else {
+      next(createHttpError(404, `User with id ${req.params.userId} not found!`))
+    }
   } catch (error) {
     next(error)
   }
@@ -39,6 +67,12 @@ usersRouter.put("/:userId", async (req, res, next) => {
 
 usersRouter.delete("/:userId", async (req, res, next) => {
   try {
+    const deletedUser = await UsersModel.findByIdAndDelete(req.params.userId)
+    if (deletedUser) {
+      res.status(204).send()
+    } else {
+      next(createHttpError(404, `User with id ${req.params.userId} not found!`))
+    }
   } catch (error) {
     next(error)
   }
